a18ca264f4b2c3fef7d4bbf3388c3a321ebcbc28
Howard Lee <contact-me@howardlee.cloud>
2020-07-11 23:41:17 +0800

    initial commit for react-ssr-example1

---
+++ webpack.server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const webpackNodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "development",
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: [webpackNodeExternals()],
+};
