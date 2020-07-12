51693932e9d948574675ba1039104dd40bd671d0
Howard Lee <contact-me@howardlee.cloud>
2020-07-12 12:34:12 +0800

    for react-ssr-example3 with supporting redux

--- src/client/components/UserList.js
+++ src/client/components/UserList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { fetchUsers } from "../redux/users/users.actions";
 
 const UserList = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <div>User List</div>
       <Link to="/">To home page</Link>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
